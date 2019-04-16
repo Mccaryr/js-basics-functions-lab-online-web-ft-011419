53875ee708cdc9c0275757934f0c2a0cc96df965
Robert McCary <robert.mccary@yahoo.com>
2019-04-16 17:52:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function calculatesFarePrice(start, destination){
   const distance = distanceTravelledInFeet(start,destination)
   if (distance < 400){
     return 0;
-  } else if (400 > distance < 2000){
+  } else if (400 >= distance <= 2000){
     return (distance - 400) * 0.02;
-  } else if (2000 < distance < 2500){
+  } else if (2000 <= distance <= 2500){
     return 25;
   } else {
     return "cannot travel that far";
