2a8541b0db86dccc5b9d794c8e5a923db96a5cd4
Robert McCary <robert.mccary@yahoo.com>
2019-04-16 17:42:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function distanceTravelledInFeet(beginning, ending){
   }
 }
 
-function calculatesFarePrice(beginning, ending){
+function calculatesFarePrice(start, destination){
   if (distanceTravelledInFeet < 400){
     return "Free ride"
   }
